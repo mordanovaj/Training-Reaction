533f790b9709eea67b5a4b2efca714559be74880
mordanovaj <mordanovajuli@icloud.com>
2025-06-23 15:01:26 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,20 @@
+"""
+запуск приложения
+"""
+import sys
+import os
+
+# Добавляем путь к src в PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+from main import ReactionTrainer
+
+if __name__ == "__main__":
+    try:
+        print("Приложение запущено")
+        app = ReactionTrainer()
+        app.run()
+        print("Выход из приложения")
+    except Exception as e:
+        import traceback
+        print(f"Возникла ошибка: {e}")
